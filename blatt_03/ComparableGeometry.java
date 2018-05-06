8928a9ef026065e8479559c1ea8943bbac59634c
tuto193 <carlos.a.parra.f@gmail.com>
2018-05-06 23:07:43 +0200

    JAVADOCHUHED

--- blatt_03/ComparableGeometry.java
+++ blatt_03/ComparableGeometry.java
@@ -1,15 +1,24 @@
 package Vererbung;
 
-
+/**
+ * Geometry objects that are able to implement Comparable
+ */
 public abstract class ComparableGeometry extends Geometry implements Comparable {
 
-	
-
+	/**
+	 * Just the default constructor. It will remain equal to the parent, apart from
+	 * the comparison methodes
+	 */
 	public ComparableGeometry(int dimension) {
 		super(dimension);
 	}
 
-	
+	/**
+	 * returns the diference of the volumes of the two geometries that were compared
+	 * 
+	 * @param o an Object, that needs to be another Geometry
+	 * @throws RuntimeException if o is not an instance of Geometry
+	 */
 	public int compareTo(Object o) {
 		if (o instanceof Geometry) {
 			return (int) ((this.volume() - ((Geometry) o).volume()));
@@ -17,5 +26,4 @@ public abstract class ComparableGeometry extends Geometry implements Comparable 
 			throw new RuntimeException("nononono");
 		}
 	}
-
 }
